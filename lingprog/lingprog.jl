c80a83761ab29f341288b9d3ff661f18778812ce
Jan Nicolas Tenorio Vieira Lima <theparadoxnick@gmail.com>
2020-05-17 11:42:50 -0300

    Add de arquivo

---
+++ lingprog/lingprog.jl
@@ -0,0 +1,43 @@
+
+# Primeira lei de Mendel
+
+#genotipoX = ["A","A"]
+#genotipoY = ["a","a"]
+
+#function combinacao(genotipoX,genotipoY)
+#    vec( collect( Base.product( zip( genotipoX, genotipoY )...) ))
+#end
+
+#combinacao(["3","1"],["2","1"])
+
+
+# function combinacao(genotipoX,genotipoY)
+#     arrayX = []
+#     arrayY = []
+#     idx = 1
+#     for char in genotipoX
+#         arrayX[idx] = char
+#         idx =+ 1
+#     end
+#     idx = ones(1)
+#     for char in genotipoY
+#         arrayY[idx] = char
+#         idx =+ 1
+#     end
+# end
+
+
+# function combinacao(genotipoX)
+#     arrayX = []
+#     idx = 1
+
+#     for char in genotipoX
+#         arrayX[idx] = char
+#         idx =+ 1
+#     end
+
+#     push!(idx)
+# end
+
+
+
